7514759ec51d0992f9d94214222ffa57b71450f7
agogo07 <augustinemutua.am@gmail.com>
2017-03-08 12:49:01 +0300

    Update solution.js

--- issue-001/solution.js
+++ issue-001/solution.js
@@ -1,3 +1,26 @@
 function dataTypeCheck(a) {
-    // implement your solution here
-}
+     if (a === +a && a % 1 === 0 ) {
+	 	alert("its an integer"); 
+        return (a / 2);
+	} else if (a === +a && a % 1 !== 0 ) {
+    	alert("its a float");
+        return a * 2;
+    } else if (typeof a === "string" && isNaN(Number(a)) === false) {
+    	alert("its a string");
+        return Number(a);
+        
+    } else if (typeof a === "string") {
+    	alert("Any other string");
+    	var abc= "hello "+a;
+    	alert(abc) ;
+        return "hello " + a;
+    } else if (typeof a !== "string" && isNaN(a) === true) {
+    	alert("Nan");
+        return 0;
+    } else if (a === null) {
+    	alert("Null");
+        return 0;
+    } else if (a === undefined) {
+    	alert("undefined");
+        return false;
+    }
