22b0c38d4f84653ab7a5e99095e78ce3768e6226
agogo07 <augustinemutua.am@gmail.com>
2017-03-08 16:16:36 +0300

    Update solution.js

--- issue-002/solution.js
+++ issue-002/solution.js
@@ -1,3 +1,13 @@
 function strictCheck(a, b) {
-    // implement your solution here
+     {
+    if(a===b){
+    	return 1; 
+    }
+    else if(a==b){
+    	return true; 
+    }
+    else if(a!=b){
+    	return false; 
+    }
+}
 }
